31684666e022239a078abf6bab47498950df2b8e
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-01 16:43:57 -0300

    feat: criar o service de login, na pasta services

---
+++ app/backend/src/services/LoginService.ts
@@ -0,0 +1,27 @@
+import * as bcrypt from 'bcryptjs';
+import ILogin from '../interfaces/LoginInterface';
+import User from '../database/models/UserModel';
+import tokenGenerate from '../helpers/TokenGenerate';
+
+class LoginService {
+  public login = async (email: string, password: string): Promise<ILogin | string> => {
+    const user = await User.findOne({ where: { email } });
+    if (!user) return ('user not found');
+    const hash = bcrypt.compareSync(password, user.password);
+    if (hash === false) return ('user not found');
+    const { id, username } = user;
+    const userToken = tokenGenerate(id, username);
+
+    return {
+      user: {
+        id,
+        username,
+        role: user.role,
+        email,
+      },
+      token: userToken,
+    } as ILogin;
+  };
+}
+
+export default LoginService;
---
+++ app/backend/src/services/UserService.ts
@@ -0,0 +1,15 @@
+// import { User } from '../database/models';
+
+// class UserService {
+//   public model = new User();
+
+//   public create = async (username: string, classe: string, level: number, password: string):
+//   Promise<string> => {
+//     const { id } = await this.model.create(username, classe, level, password);
+//     // const userToken = tokenGenerate(id, username);
+
+//     return userToken;
+//   };
+// }
+
+// export default UserService;
