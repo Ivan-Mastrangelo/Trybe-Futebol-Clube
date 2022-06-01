dc309d773a1901bf086dafc51fa09868ba8bab1b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-01 18:30:44 -0300

    fix: retirar validação de email do joi

--- app/backend/src/middlewares/ValidateUserLogin.ts
+++ app/backend/src/middlewares/ValidateUserLogin.ts
@@ -3,10 +3,9 @@ import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const productSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string().required(),
   password: Joi.string().min(6).required(),
 }).messages({
-  'string.email': 'Incorrect email or password',
   'any.required': 'All fields must be filled' });
 
 const validateUserLogin = (req: Request, res: Response, next: NextFunction) => {
