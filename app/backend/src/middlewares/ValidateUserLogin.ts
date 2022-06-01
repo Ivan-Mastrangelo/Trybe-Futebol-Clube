7df80a8e58d09287c0883d9089831a8481413183
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-01 17:28:58 -0300

    fix: rearrumando login validate

--- app/backend/src/middlewares/ValidateUserLogin.ts
+++ app/backend/src/middlewares/ValidateUserLogin.ts
@@ -5,16 +5,17 @@ import { StatusCodes } from 'http-status-codes';
 const productSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-});
+}).messages({
+  'string.email': 'Incorrect email or password',
+  'any.required': 'All fields must be filled' });
 
 const validateUserLogin = (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body;
     const { error } = productSchema.validate({ email, password });
-    console.log(error);
     if (error) {
       return res.status(StatusCodes.BAD_REQUEST)
-        .json({ message: 'All fields must be filled' });
+        .json({ message: error.message });
     }
 
     next();
