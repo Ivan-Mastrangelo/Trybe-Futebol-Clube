cbb48c972f757d9cf9ed028869964f57e58d6db8
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-01 19:12:47 -0300

    refactor: Excluir mensegem duplicada no arquivo ValidateUserLogin

--- app/backend/src/middlewares/ValidateUserLogin.ts
+++ app/backend/src/middlewares/ValidateUserLogin.ts
@@ -5,8 +5,7 @@ import { StatusCodes } from 'http-status-codes';
 const productSchema = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().min(6).required(),
-}).messages({
-  'any.required': 'All fields must be filled' });
+});
 
 const validateUserLogin = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -14,7 +13,7 @@ const validateUserLogin = (req: Request, res: Response, next: NextFunction) => {
     const { error } = productSchema.validate({ email, password });
     if (error) {
       return res.status(StatusCodes.BAD_REQUEST)
-        .json({ message: error.message });
+        .json({ message: 'All fields must be filled' });
     }
 
     next();
