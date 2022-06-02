2905890c99e3c18c058b15b98537e5725a6fb30f
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-02 00:12:29 -0300

    feat: criar a /login/validate no LoginRoutes

--- app/backend/src/routes/LoginRoutes.ts
+++ app/backend/src/routes/LoginRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import tokenVerify from '../middlewares/ValidateToken';
 import LoginController from '../controllers/LoginController';
 import validateUserLogin from '../middlewares/ValidateUserLogin';
 
@@ -7,5 +8,6 @@ const loginController = new LoginController();
 const route = Router();
 
 route.post('/', validateUserLogin, loginController.login);
+route.get('/validate', tokenVerify, loginController.loginValidate);
 
 export default route;
