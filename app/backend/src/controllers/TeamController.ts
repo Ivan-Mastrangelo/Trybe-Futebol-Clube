9295d0d9635811e678a7220bee255cf60a91e32d
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-04 17:40:58 -0300

    feat: implementar a TeamController

---
+++ app/backend/src/controllers/TeamController.ts
@@ -0,0 +1,20 @@
+import { StatusCodes } from 'http-status-codes';
+import { Request, Response, NextFunction } from 'express';
+import TeamService from '../services/TeamService';
+
+class TeamController {
+  public service = new TeamService();
+
+  public getAllTeams = async (_req: Request, res: Response, next: NextFunction):
+  Promise<Response | void> => {
+    try {
+      const allTeams = await this.service.getAllTeams();
+
+      return res.status(StatusCodes.OK).json(allTeams);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default TeamController;
