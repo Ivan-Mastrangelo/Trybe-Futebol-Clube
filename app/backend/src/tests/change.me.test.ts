5d819deeb22e76f30a4de399df9ecdd2d85a0f9f
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-03 21:41:00 -0300

    feat: escrever os testes de cobertura no arquivo src/tests/change.me.test.ts

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -2,44 +2,190 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import * as jwt from 'jsonwebtoken';
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import User from '../database/models/UserModel';
 
 import { Response } from 'superagent';
+import TokenGenerate from '../helpers/TokenGenerate';
+
+import { invalid } from 'joi';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('testar a integridade da rota /login', () => {
+  describe('Em caso de sucesso', () => { 
 
-  // let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+  before(async () => {
+    sinon
+      .stub(User, 'findOne')
+      .resolves({
+          id: 1,
+          username: 'Admin',
+          role: 'admin',
+          email: 'admin@admin',
+          password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+      } as User);
+  });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  after(() => {
+    (User.findOne as sinon.SinonStub).restore();
+  });
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  it('Com email e password corretos', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send({
+        email: 'admin@admin.com',
+        password: 'secret_admin'
+       });
 
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.have.property('user');
+    expect(chaiHttpResponse.body).to.have.property('token');
   });
 });
+  describe('Em casos de erros', () => {
+    let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, 'findOne')
+      .resolves();
+  });
+
+  after(() => {
+    (User.findOne as sinon.SinonStub).restore();
+  });
+
+    it('Em caso da requisição vir com email inválido', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/login')
+         .send({
+           password: 'secret_admin',
+           email: 'adm@admin.com'
+         });
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body.message).to.be.equal('Incorrect email or password');
+    });
+
+    it('Em caso da requisição vir com Password inválido', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/login')
+         .send({
+           password: 'sect_admin',
+           email: 'admin@admin.com'
+         });
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body.message).to.be.equal('Incorrect email or password');
+    });
+
+    it('Em caso da requisição vir sem o campo email', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/login')
+         .send({
+           password: 'secret_admin'
+         });
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.body.message).to.be.equal('All fields must be filled');
+    });
+
+    it('Em caso da requisição vir sem o campo password', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/login')
+         .send({
+          email: 'admin@admin.com'
+         });
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.body.message).to.be.equal('All fields must be filled');
+    });
+  });
+
+  describe('testar a rota /loginValidate da rota login', () => {
+    describe('Em casos de sucesso', () => {
+
+      let chaiHttpResponse: Response;
+
+      before(async () => {
+        sinon
+          .stub(User, 'findByPk')
+          .resolves({
+              id: 1,
+              username: 'Admin',
+              role: 'admin',
+              email: 'admin@admin',
+              password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+          } as User);
+        sinon.stub(jwt, 'verify').resolves({ id: 1 })
+      });
+    
+      after(() => {
+        (User.findByPk as sinon.SinonStub).restore();
+        (jwt.verify as sinon.SinonStub).restore();
+      });
+
+      it('Requisição com o token correto', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/login/validate')
+          .set('authorization', 'token')
+          .send();
+        expect(chaiHttpResponse.status).to.be.equal(200);
+        expect(chaiHttpResponse.body).to.be.equal('admin');
+      });
+    });
+
+    describe('Em casos de erro', () => {
+
+      let chaiHttpResponse: Response;
+
+      before(async () => {
+        sinon
+          .stub(User, 'findByPk')
+          .resolves({
+            id: 1,
+            username: 'Admin',
+            role: 'admin',
+            email: 'admin@admin',
+            password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+        } as User);
+      });
+    
+      after(() => {
+        (User.findByPk as sinon.SinonStub).restore();
+      });
+
+      it('Em caso da requisição vir sem o token', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/login/validate')
+          .set('authorization', '')
+          .send();
+        expect(chaiHttpResponse.status).to.be.equal(401);
+        expect(chaiHttpResponse.body.message).to.be.equal('Token not found');
+      });
+
+      it('Em caso da requisição vir com o token inválido', async () => {
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/login/validate')
+          .set('authorization', 'token')
+          .send();
+        expect(chaiHttpResponse.status).to.be.equal(401);
+        expect(chaiHttpResponse.body.message).to.be.equal('Invalid token');
+      });
+    });
+  });
+});
+
