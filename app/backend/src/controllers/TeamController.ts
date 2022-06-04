281eb5b415654a611cc1c22d92df4406081ec130
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-04 18:34:25 -0300

    feat: implementar o método getById na TeamController

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -1,16 +1,29 @@
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response, NextFunction } from 'express';
 import TeamService from '../services/TeamService';
+import { number, string } from 'joi';
 
 class TeamController {
   public service = new TeamService();
 
-  public getAllTeams = async (_req: Request, res: Response, next: NextFunction):
-  Promise<Response | void> => {
+  public getAllTeams = async (_req: Request, res: Response, next: NextFunction)
+  :Promise<Response | void> => {
     try {
       const allTeams = await this.service.getAllTeams();
 
       return res.status(StatusCodes.OK).json(allTeams);
+    } catch (error) {
+      next(error);
+    };
+  };
+
+  public getTeamById = async (req: Request, res: Response, next: NextFunction)
+  :Promise<Response | void> => {
+    try {
+      const { id } = req.params;
+      const team = await this.service.getTeamById(+id);
+
+      return res.status(StatusCodes.OK).json(team);
     } catch (error) {
       next(error);
     }
