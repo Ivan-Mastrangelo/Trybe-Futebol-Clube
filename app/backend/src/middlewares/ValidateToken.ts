a12c94420d9aa7f23d60abe126d383ff23584789
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-03 21:39:52 -0300

    refactor: alterar importação do verify para importação do jwt e modificar o uso do verify para jwt.verify no arquivo ValidateToken, para poder utilizar o moch da função nos testes

--- app/backend/src/middlewares/ValidateToken.ts
+++ app/backend/src/middlewares/ValidateToken.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import { Request, Response, NextFunction } from 'express';
-import { verify } from 'jsonwebtoken';
+import jwt = require('jsonwebtoken');
 import { StatusCodes } from 'http-status-codes';
 import IToken from '../interfaces/TokenInterface';
 
@@ -12,7 +12,7 @@ const tokenVerify = (req: Request, res: Response, next: NextFunction) => {
       return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token not found' });
     }
     const secretKey = fs.readFileSync('jwt.evaluation.key');
-    const payload = verify(token, secretKey);
+    const payload = jwt.verify(token, secretKey);
     const newPayload = payload as IToken;
     req.body.user = newPayload.id;
     next();
