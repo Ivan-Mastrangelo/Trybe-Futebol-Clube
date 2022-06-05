bb360e0ae221768e6e80f732e29dae00be479426
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-05 14:35:06 -0300

    feat: implementar condicionais para encaminhar pesquisas ao banco por partidas em andamento ou finalizadas, na MatcheController

--- app/backend/src/controllers/MatcheController.ts
+++ app/backend/src/controllers/MatcheController.ts
@@ -5,9 +5,19 @@ import MatcheService from '../services/MatcheService';
 class MatcheController {
   public service = new MatcheService();
 
-  public getAllMatches = async (_req: Request, res: Response, next: NextFunction)
+  public getAllMatches = async (req: Request, res: Response, next: NextFunction)
   :Promise<Response | void> => {
     try {
+      if (req.query.inProgress === 'true') {
+        const allMatchesInProgress = await this.service.getAllMatchesInProgress();
+        return res.status(StatusCodes.OK).json(allMatchesInProgress);
+      }
+
+      if (req.query.inProgress === 'false') {
+        const allFinishedMatches = await this.service.getAllFinishedMatches();
+        return res.status(StatusCodes.OK).json(allFinishedMatches);
+      }
+
       const allMatches = await this.service.getAllMatches();
 
       return res.status(StatusCodes.OK).json(allMatches);
