a0644958457ad1710b01c5261f008fdc88e9a0b8
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-05 19:23:49 -0300

    feat: implementar as rotas post e patch para MatcheRoutes

--- app/backend/src/routes/MatchesRoutes.ts
+++ app/backend/src/routes/MatchesRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import MatcheController from '../controllers/MatcheController';
+import tokenVerify from '../middlewares/ValidateToken';
 
 const matcheController = new MatcheController();
 
 const route = Router();
 
 route.get('/', matcheController.getAllMatches);
+route.post('/', tokenVerify, matcheController.createMatcheInProgress);
+route.patch('/:id/finish', matcheController.setMatcheToFinished);
 
 export default route;
